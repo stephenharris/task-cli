d38048c2f3cd56f2fe8ffd6231c65c7b139445bb
Stephen Harris <contact@stephenharris.info>
2022-12-08 19:53:18 +0000

    make entry.sh executable

--- scripts/build-package.js
+++ scripts/build-package.js
@@ -1,4 +1,5 @@
-import {copyFileSync} from 'fs';
+import {copyFileSync, chmodSync} from 'fs';
 
 copyFileSync('package.json', 'build/package.json');
 copyFileSync('src/entry.sh', 'build/entry.sh');
+chmodSync('build/entry.sh', '755');
