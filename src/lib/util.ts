9a875e439b254da4d64977c1010c14e77990aae8
Stephen Harris <stephen.harris1@kaluza.com>
2022-12-23 16:34:00 +0000

    Move deepEqual to util file

---
+++ src/lib/util.ts
@@ -0,0 +1,7 @@
+export function deepEqual(x: any, y: any): boolean {
+    const ok = Object.keys, tx = typeof x, ty = typeof y;
+    return x && y && tx === 'object' && tx === ty ? (
+      ok(x).length === ok(y).length &&
+        ok(x).every((key: any) => deepEqual(x[key], y[key]))
+    ) : (x === y);
+  }
